8e2ba48dea975a8e50c3023dac22eb4e49772a38
“““SYSU-Winter git config --global user.email “Yliuwt1314@gmail.com <liuwt1314@gmail.com>
2017-04-01 15:42:45 +0800

    add parseXmlWithPull

--- WebTechnique/app/src/main/java/sysu/lwt/webtechnique/MainActivity.java
+++ WebTechnique/app/src/main/java/sysu/lwt/webtechnique/MainActivity.java
@@ -6,11 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -55,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 HttpsURLConnection connection = null;
                 BufferedReader reader = null;
                 try {
-                    URL url = new URL("https://www.baidu.com");
+                    URL url = new URL("http://ws.webxml.com.cn/WebServices/WeatherWS.asmx/getWeather?theCityCode=广州&theUserID=");
                     connection = (HttpsURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
@@ -69,7 +76,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     while ((line = reader.readLine()) != null) {
                         builder.append(line);
                     }
-                    showResponse(builder.toString());
+                    //showResponse(builder.toString());
+                    parseXMLWithPull(builder.toString());
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
@@ -97,13 +105,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     OkHttpClient client = new OkHttpClient();
                     // 创建一个Request对象，这是发起一条HTTP请求的前提
                     Request request = new Request.Builder()
-                            .url("https://baidu.com")
+                            .url("http://ws.webxml.com.cn/WebServices/WeatherWS.asmx/getWeather?theCityCode=广州&theUserID=")
                             .build();
                     // 调用OkHttpClient的newCall()方法来创建一个Call对象，并调用它的execute方法
                     // 来发送请求并获取服务器返回的数据
                     Response response = client.newCall(request).execute();
                     String responseDate = response.body().string();
-                    showResponse(responseDate);
+                    //showResponse(responseDate);
+                    parseXMLWithPull(responseDate);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -111,13 +120,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }).start();
     }
 
-    private void showResponse(final String response) {
+    private void showResponse(final List<String> list) {
         // 将线程切回主线程，更新UI
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                text.setText(response);
+                String result = "";
+                for (int i = 0; i < list.size(); i++) {
+                    result += list.get(i);
+                    result += "\n";
+                }
+                text.setText(result);
             }
         });
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            // 首先是创建一个XmlPullParseFactory的实例
+            XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
+            // 借助这个实例得到XmlPullParser对象
+            XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
+            // 调用XmlPullParser的setInput()方法将服务器返回的XML数据设置进去，然后就可以解析了
+            xmlPullParser.setInput(new StringReader(xmlData));
+
+            // 通过getEventType()得到当前的解析事件，然后在一个while循环中不断的进行解析
+            // 如果当前的解析事件不等于XmlPullParser。END_DOCUMENT, 说明解析工作还没有完成
+            // 调用next()方法获取下一个解析事件
+            int eventType = xmlPullParser.getEventType();
+            List<String> list = new ArrayList<>();
+            // 在while循环中，通过getName()来获取当前节点的名字，如果发现节点等于string
+            // 就调用nextText()来获取节点的具体内容
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    // 开始解析某个节点
+                    case XmlPullParser.START_TAG:
+                        String name = xmlPullParser.getName();
+                        if ("string".equals(name)) {
+                            String str = xmlPullParser.nextText();
+                            list.add(str);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+            showResponse(list);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
