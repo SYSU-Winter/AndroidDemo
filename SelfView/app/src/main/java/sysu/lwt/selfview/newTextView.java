6764bca20f3e908ef3bdea8a58c0b83bb2093824
Winter <liuwt1314@gmail.com>
2017-05-19 21:06:01 +0800

    add readme

---
+++ SelfView/app/src/main/java/sysu/lwt/selfview/newTextView.java
@@ -0,0 +1,20 @@
+package sysu.lwt.selfview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by 12136 on 2017/5/17.
+ */
+
+public class newTextView extends View {
+    public newTextView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
+}
